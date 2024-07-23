93c44569ebadd6cb8be9a4bf5f42bef6e6149ae1
Eduardo Macêdo <eduardojuniodev@gmail.com>
2024-07-22 23:07:41 -0300

    Add shooting mechanic

---
+++ player-bullet.gd
@@ -0,0 +1,19 @@
+extends Sprite2D
+
+var speed = 1500 # in pixels per second
+var direction = Vector2.ZERO # Direction the bullet will travel
+
+func _process(delta):
+	global_position += direction * speed * delta
+	if is_out_of_screen():
+		queue_free()
+
+func is_out_of_screen():
+	return not get_viewport_rect().intersects(get_global_rect())
+
+func get_global_rect():
+	return Rect2(global_position - (get_size() / 2), get_size())
+
+# TODO: Duplicated from player script
+func get_size():
+	return get_rect().size
--- player.gd
+++ player.gd
@@ -1,17 +1,25 @@
 extends Sprite2D
 
-var speed = 300 # in pixels per second
+var PlayerBullet = preload("res://player-bullet.tscn")
 
-func _input(event):
+var speed = 300 # in pixels per second
+var shooting = false
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate_sprite(event.position)
 	if event is InputEventMouseButton:
-		print()
+		shooting = event.pressed
 
 func _process(delta):
 	move(delta)
+	if shooting:
+		shoot(get_global_mouse_position())
 
 func move(delta):
 	position += get_motion() * speed * delta
 	clamp_position()
+	rotate_sprite(get_global_mouse_position())
 
 func clamp_position():
 	var min = get_size() / 2
@@ -34,4 +42,34 @@ func get_size():
 	return get_rect().size
 
 func get_viewport_size():
-	return get_viewport_rect().size
+	return get_viewport().get_visible_rect().size
+
+func rotate_sprite(target_position):
+	rotation = get_direction_rotation(get_direction(target_position))
+
+func shoot(target_position):
+	var cooldown = $Cooldown
+
+	if not cooldown.is_stopped():
+		return
+
+	var bullet_instance = PlayerBullet.instantiate()
+	get_parent().add_child(bullet_instance)
+
+	var bullet_direction = get_direction(target_position)
+	bullet_instance.direction = bullet_direction
+	bullet_instance.rotation = get_direction_rotation(bullet_direction)
+	var bullet_size_offset = (bullet_instance.get_size().x / 2) * bullet_direction
+	var extra_offset = 10 * bullet_direction
+	bullet_instance.global_position = global_position + (bullet_direction * get_radius()) + bullet_size_offset + extra_offset
+
+	cooldown.start()
+
+func get_direction(target_position):
+	return (target_position - global_position).normalized()
+
+func get_direction_rotation(direction):
+	return Vector2.RIGHT.angle_to(direction)
+
+func get_radius():
+	return get_size().x / 2
