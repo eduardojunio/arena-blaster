b2d89018301cdbc9774ddee11431559b830853f6
Eduardo Macêdo <eduardojuniodev@gmail.com>
2024-07-23 22:22:15 -0300

    Add health bar

--- main.gd
+++ main.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
+@export var crosshair_texture: Texture2D
+@export var crosshair_size: Vector2
+
 func _ready():
 	set_crosshair()
 
 func set_crosshair():
-	var crosshair_texture = preload("res://crosshair.png")
-	var crosshair_size = Vector2(25, 25)
 	Input.set_custom_mouse_cursor(crosshair_texture, 0, crosshair_size / 2)
