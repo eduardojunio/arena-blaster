21676c0dfae1e318f1bf6640cb8f58e22e913269
Eduardo Macêdo <eduardojuniodev@gmail.com>
2024-08-06 14:55:08 -0300

    Add speed boost drop

---
+++ scripts/speed-boost.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+@export var speed_increase = 200 # in pixels per second
+
+@onready var timer = $Timer
+@onready var collision = $CollisionShape2D
+
+var boosted_body = null
+
+func _ready():
+	body_entered.connect(increase_speed)
+	timer.timeout.connect(decrease_speed)
+
+func increase_speed(body):
+	var movement_component = get_movement_component(body)
+	if timer.is_stopped() and movement_component:
+		movement_component.speed += speed_increase
+		boosted_body = body
+		timer.start()
+		hide()
+
+func decrease_speed():
+	if boosted_body:
+		var movement_component = get_movement_component(boosted_body)
+		movement_component.speed -= speed_increase
+		queue_free()
+
+func get_movement_component(body):
+	if body.has_node("MovementComponent"):
+		return body.get_node("MovementComponent")
