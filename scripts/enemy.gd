d4a188a22215eb20798c86f8473fcb48ac49bd85
Eduardo Macêdo <eduardojuniodev@gmail.com>
2024-08-06 14:55:08 -0300

    Add speed boost drop

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -4,6 +4,8 @@ class_name Enemy extends Node2D
 @export var health_pack: PackedScene
 @export var health_pack_chance = 0 # in percentage
 @export var health_pack_critical_chance = 0 # in percentage
+@export var speed_boost: PackedScene
+@export var speed_boost_chance = 0 # in percentage
 
 @onready var health_component = $HealthComponent
 @onready var movement_component = $MovementComponent
@@ -17,6 +19,8 @@ func handle_health_change(value, current_health):
 	if current_health == 0:
 		if should_spawn_health_pack():
 			spawn_health_pack()
+		elif should_spawn_speed_boost():
+			spawn_speed_boost()
 		queue_free()
 	elif has_node("FlashingComponent"):
 		$FlashingComponent.start_flashing()
@@ -26,7 +30,13 @@ func handle_collision(collision):
 		player.get_node("HealthComponent").die()
 
 func should_spawn_health_pack():
-	return randf() < (get_health_pack_chance() / 100.0)
+	return roll(get_health_pack_chance())
+
+func should_spawn_speed_boost():
+	return roll(speed_boost_chance)
+
+func roll(percentage):
+	return randf() < (percentage / 100.0)
 
 func get_health_pack_chance():
 	var player_health_percentage = player.get_node("HealthComponent").get_health_percentage()
@@ -39,3 +49,8 @@ func spawn_health_pack():
 	var health_pack_instance = health_pack.instantiate()
 	health_pack_instance.global_position = global_position
 	get_tree().root.add_child(health_pack_instance)
+
+func spawn_speed_boost():
+	var speed_boost_instance = speed_boost.instantiate()
+	speed_boost_instance.global_position = global_position
+	get_tree().root.add_child(speed_boost_instance)
